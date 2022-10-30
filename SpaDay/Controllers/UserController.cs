f86aaf2c560a8ee33832fd47f4915c4a9603507e
lenakonik <alphabetweb@gmail.com>
2022-10-30 17:08:33 -0400

    done

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System.Collections.Generic;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/User/Add")]
+        public IActionResult Add()
+        
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password.Equals(verify))
+            {
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            }
+            ViewBag.username = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.error = "Your passwords should match";
+            return View("Add");
+
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+
+}
